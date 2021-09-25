fcb26a0c314a0ab83a7a9eb87ed312db1e04aa3f
kalam ahmed <kalamahmed714@gmail.com>
2021-09-25 22:12:04 +0600

    functional and class components

---
+++ src/components/Class.js
@@ -0,0 +1,10 @@
+
+import React, { Component } from "react";
+class Hellow extends Component {
+    render(){
+        return <h1>Wellcome To Rect Js World.From Class components</h1>
+
+    }
+
+}
+export default Hellow
---
+++ src/components/Functional.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Hi(){
+    return <h1>Wellcome To Rect Js World.From Functional components</h1>
+}
+export default Hi
