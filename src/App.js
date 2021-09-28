a3afd1502bb9836c2a26e8b9657f09f20f66a9a7
kalam ahmed <kalamahmed714@gmail.com>
2021-09-28 19:50:06 +0600

    form validation,error showing,select option,list

--- src/App.js
+++ src/App.js
@@ -34,11 +34,18 @@ import './App.css';
 
 // export default App;
  class App extends React.Component{
-   state = {
-     username:'',
-     email:'',
-     adress:'',
-   }
+  //  state = {
+  //   //  username:'',
+  //   //  email:'',
+  //   //  adress:'',
+  //   email:'',
+  //   password:'',
+  //   error:'',
+  //   errorEmail:'',
+  //   errorPassword:'',
+  //   desc:'',
+  //   kdrama:''
+  //  }
   //  textGen = (e)=>{
   //    let value = e.target.value
   //    this.setState({text:value})
@@ -54,24 +61,50 @@ import './App.css';
   //   console.log(this.state.text) 
   // }
   
-  textGen =(e)=>{
-    let val= e.target.name
-    let tarValue=e.target.value
-    this.setState({[val]:tarValue})
+  // textGen =(e)=>{
+  //   let val= e.target.name
+  //   let tarValue=e.target.value
+  //   this.setState({[val]:tarValue})
 
-  }
+  // } *multiple 
+  
+  // validate(){
+  //   if(!this.state.email>0 && !this.state.password>0){
+  //     this.setState({error:'Email and Password fields cannot be empty'})
+
+  //   } else if(!this.state.email >0){
+  //     this.setState({emailError:'Email field cannot be empty'})
+  //   }else if(!this.state.password >0){
+  //     this.setState({passwordError:'Password field cannot be empty'})
+  //   }else{
+  //     return true
+  //   }
+  // }
+
+  // formSubmit = (e)=>{
+  //   e.preventDefault()
+  //   if(this.validate()){
+  //     alert('login information matched with database')
+
+  //   }
+  // }
   render(){
+    const movies=['Inception','Endgame','Infinity War','Thor']
+    const names=movies.map((name,id)=>{
+      return <li>{id+1} {name}</li>
+    })
+
     return (
       <div className="App"> 
         <h1>System Time {new Date().toLocaleTimeString()}</h1>
         {/* <h4>{this.state.text}</h4> */}
         {/* <form onSubmit={this.textSubmit}> */}
 
-        <h5>{this.state.username}</h5>
+        {/* <h5>{this.state.username}</h5>
         <h5>{this.state.email}</h5>
 
-        <h5>{this.state.address}</h5>
-
+        <h5>{this.state.address}</h5> */}
+{/* 
         <form>
         <div class='form-group'>
           <label>User Name : </label>
@@ -86,7 +119,39 @@ import './App.css';
            <button class='btn btn-primary' type='submit'>Submit</button>
 
         </div>
+        </form> */}
+
+{/* <h5>{this.state.email}</h5>
+<h5>{this.state.password}</h5> */}
+{/* <h2> Login System </h2>
+
+        <form onSubmit={this.formSubmit}>
+          <strong style={{color:'red'}}>{this.state.error}</strong><br></br>
+        <label>Email : </label>
+          <input type='email' name='email' onChange={(e)=>{this.setState({email:e.target.value})}}></input><br></br>
+              <strong style={{color:'red'}}>{this.state.emailError}</strong><br></br>
+          <label>password: </label>
+          <input type='password' name='password' onChange={(e)=>{this.setState({password:e.target.value})}}></input><br></br>
+          <strong style={{color:'red'}}>{this.state.passwordError}</strong><br></br>
+          <textarea onChange={(e)=>{this.setState({desc:e.target.value})}}></textarea><br></br>
+          <p>{this.state.desc}</p><br></br><br></br>
+          <select onChange={(e)=>{this.setState({kdrama:e.target.value})}}> 
+            <option selected disabled>Select Your Fav Kdrama</option>
+            <option value='Mouse 2021'>Mouse 2021</option>
+            <option value='Whats wrong with secretary kim'>What's Wrong with secretary Kim</option>
+            <option value='Its ok to not to be okey'>Its ok to not to be okey</option>
+
+          </select><br></br>
+          <h3>My fav kdrama is : {this.state.kdrama}</h3>
+
+            <button type='submit' class='btn btn-success'>Submit</button>
         </form>
+         */}
+         <div>
+           <h1>Movie List</h1>
+           {names}
+
+         </div>
         </div>
     )
   }
