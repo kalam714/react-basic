b02fdbd5dcd0559fc4fde0926b03451064a348ac
kalam ahmed <kalamahmed714@gmail.com>
2021-10-01 21:43:02 +0600

    fetch data from api

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
+const axios = require('axios');
+
 
 export default class Post extends Component{
+    state = {
+        posts:[]
+    }
+    componentDidMount(){
+        // alert('check component load')
+      //  let thisVar=this
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) =>{
+           //     thisVar.setState({posts:response.data})
+           this.setState({posts:response.data})
+      
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+    }
+
     render(){
+        const posts=this.state.posts
+        const allPosts=posts.map((post,id)=>{
+            return(
+                <div>
+                    <a href="" key={id}>{post.title}</a><br></br>
+                    <p>{post.body}</p>
+                </div>
+            )
+        })
+
         return(
             <div>
-                <h2>Post Component</h2>
-            </div>
+<div class="media">
+  <div class="media-body text-center">
+      {allPosts}
+  </div>
+</div>            </div>
         )
     }
 }
